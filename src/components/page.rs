8fb18016585ead04eb728cfb104ed098e6e38eba
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-09 21:18:59 +0200

    Fixed static issue

--- src/components/page.rs
+++ src/components/page.rs
@@ -7,5 +7,5 @@ use leptos::{
 pub fn Page() -> impl IntoView {
     div()
         .class("rounded bg-orange-100 border")
-        .child(img().src("/static/images/profile.png"))
+        .child(img().src("./static/images/profile.png"))
 }
