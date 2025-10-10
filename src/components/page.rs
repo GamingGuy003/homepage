6500638e6183c95a9eb7fe9d5cee26bf0de1546e
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-10 11:44:33 +0200

    Added basic identity styling

--- src/components/page.rs
+++ src/components/page.rs
@@ -6,6 +6,11 @@ use leptos::{
 #[component]
 pub fn Page() -> impl IntoView {
     div()
-        .class("rounded bg-orange-100 border")
-        .child(img().src("./static/images/profile.png"))
+        .class("rounded bg-orange-100 p-5 m-5 flex gap-5 shadow-md")
+        .child((
+            img()
+                .src("./static/images/profile.png")
+                .class("rounded shadow-md"),
+            div().class("flex-col flex w-full rounded bg-yellow-100 shadow-md"),
+        ))
 }
