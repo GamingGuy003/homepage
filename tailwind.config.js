d62ab82685f9ac25a542d7c27f949e7ee03d8c6a
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-11 19:41:59 +0200

    Finally fixed tailwind styling??

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.rs", "./src/*.rs"],
-  theme: {
-    extend: {
-      colors: {
-        'storm-black': '#24283b',
-        'storm-night': '#1a1b26',
-        'storm-white': '#a9b1d6',
-        'terminal-red': '#f7768e',
-        'terminal-orange': '#ff9e64',
-        'terminal-yellow': '#e0af68',
-        'terminal-green': '#9ece6a',
-        'terminal-azure': '#2ac3de',
-        'terminal-purple': '#bb9af7',
-      }
-    }
-  },
+  theme: { extend: {}},
   darkMode: 'class',
   plugins: [],
 }
