29f08b9f3dec5d6f79da9133f28b113034f3ea69
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-11 16:50:54 +0200

    Added scroll progress bar

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+fn main() {
+    // create GIT_HASH env variable for output in page
+    let git_output = Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output()
+        .unwrap();
+    // create DATE env variable for output in page
+    let date_output = Command::new("date").output().unwrap();
+
+    let git_hash = String::from_utf8(git_output.stdout).unwrap();
+    let date = String::from_utf8(date_output.stdout).unwrap();
+    println!("cargo:rustc-env=GIT_HASH={git_hash}");
+    println!("cargo:rustc-env=DATE={date}");
+}
