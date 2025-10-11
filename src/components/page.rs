b74228ec41d8ffd0b8eb67a624b198f64ae9fc36
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-12 00:20:31 +0200

    Improved theme, added darkmode toggle

--- src/components/page.rs
+++ src/components/page.rs
@@ -1,44 +1,36 @@
 use leptos::{
     ev,
-    html::{div, footer, img, nav, progress},
+    html::{div, footer},
     prelude::*,
 };
+use leptos_darkmode::Darkmode;
+use leptos_meta::provide_meta_context;
+
+use crate::components::nav::Nav;
 
 #[component]
 pub fn Page() -> impl IntoView {
-    let (scrollprogress, set_scrollprogress) = signal(0.0);
-
-    window_event_listener(ev::scroll, move |_| {
-        let window = web_sys::window().unwrap();
-        let document = window.document().unwrap();
-        let element = document.document_element().unwrap();
-
-        let scroll_top = element.scroll_top() as f64;
-        let scroll_height = element.scroll_height() as f64;
-        let client_height = element.client_height() as f64;
-
-        set_scrollprogress.set((scroll_top / (scroll_height - client_height)) * 100.0);
-    });
+    provide_meta_context();
+    let darkmode = Darkmode::init();
 
     div()
         .child((
-            nav()
-                .child(
-                    div().class("flex-col flex dark:bg-storm-night").child((
-                        img().src("./static/images/profile.png").class("size-12"),
-                        progress()
-                            .class("h-0.5")
-                            .max(100)
-                            .value(move || scrollprogress.get()),
-                    )),
-                )
-                .class("shadow-md fixed w-full"),
-            div().style("height: 200vh").class("shadow-md"),
-            footer().child(format!(
-                "Commit {} built on {}",
-                env!("GIT_HASH"),
-                env!("DATE")
-            )),
+            // navbar
+            Nav(),
+            // content
+            div()
+                .style("height: 200vh")
+                .class("shadow-md bg-background-content dark:bg-background-content-dark"),
+            // footer
+            footer()
+                .child(format!(
+                    "commit {} built on {}",
+                    env!("GIT_HASH"),
+                    env!("DATE")
+                ))
+                .class("bg-background-ui dark:bg-background-ui-dark text-foreground dark:text-foreground-dark h-20 place-self-center"),
         ))
         .on(ev::click, |data| leptos::logging::log!("{:?}", data))
+        // darkmode toggle
+        .class(move || darkmode.is_dark().then(|| Some("dark")))
 }
