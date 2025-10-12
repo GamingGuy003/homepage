c8c8d31860a52b1342c9045213307f8862b8d1d8
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-12 19:25:53 +0200

    Forgot path

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ fn main() {
     let children = TypedChildren::to_children(Page);
     let props = RouterProps::builder()
         .children(children)
-        //        .base(option_env!("BASE_PATH").unwrap_or("/#/"))
-        .base("/sond/#")
+        .base(option_env!("BASE_PATH").unwrap_or(""))
         .build();
 
     leptos::mount::mount_to_body(|| Router(props));
