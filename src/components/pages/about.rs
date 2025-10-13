0e10e0e5a26549ed45f2795bc1fa7a9f882bd478
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-13 02:08:20 +0200

    Added tooltips

--- src/components/pages/about.rs
+++ src/components/pages/about.rs
@@ -175,28 +175,36 @@ pub fn Languages() -> impl IntoView {
                         .child("Knowledge"),
                     div()
                         .class("absolute bottom-[90%] left-[85%] -translate-x-1/2")
-                        .child("Rust"),
+                        .child("Rust")
+                        .title("Fun if you like fighting your compiler"),
                     div()
                         .class("absolute bottom-[30%] left-[65%] -translate-x-1/2")
-                        .child("JS"),
+                        .child("JS")
+                        .title("Dynamic types will be humanities downfall"),
                     div()
                         .class("absolute bottom-[50%] left-[70%] -translate-x-1/2")
-                        .child("TS"),
+                        .child("TS")
+                        .title("Can't be worse than JS"),
                     div()
                         .class("absolute bottom-[55%] left-[60%] -translate-x-1/2")
-                        .child("C"),
+                        .child("C")
+                        .title("Fun but requires 200IQ"),
                     div()
                         .class("absolute bottom-[40%] left-[50%] -translate-x-1/2")
-                        .child("CPP"),
+                        .child("CPP")
+                        .title("Bloated"),
                     div()
                         .class("absolute bottom-[60%] left-[35%] -translate-x-1/2")
-                        .child("Python"),
+                        .child("Python")
+                        .title("Rarely used but fun if typed"),
                     div()
                         .class("absolute bottom-[60%] left-[75%] -translate-x-1/2")
-                        .child("Java"),
+                        .child("Java")
+                        .title("Used lots, could be worse"),
                     div()
                         .class("absolute bottom-[80%] left-[20%] -translate-x-1/2")
-                        .child("Lua"),
+                        .child("Lua")
+                        .title("Rarely used"),
                 ))
         ))
 }
