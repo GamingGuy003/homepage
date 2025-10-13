b10f14f7b2c5b29ddf61c058ca71007f9f0f3246
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-13 15:45:46 +0200

    Made modules cleaner to make clippy happy

--- src/components/pages/landing/mod.rs
+++ src/components/pages/landing/mod.rs
@@ -1,1 +1,65 @@
-pub mod landing;
+use std::sync::Arc;
+
+use leptos::{
+    html::{a, div, p},
+    prelude::*,
+};
+use leptos_icons::{Icon, IconProps};
+
+use crate::{
+    based_url,
+    components::pages::about::card::{Card, CardProps},
+};
+
+#[component]
+pub fn Landing() -> impl IntoView {
+    div()
+        .class("mt-[15vh] grid gap-3 max-h-screen place-items-center")
+        .child((
+            div()
+                .class(
+                    "col-span-1 md:col-span-2 justify-center flex bg-gradient-to-br
+                    from-terminal-red-dark dark:from-terminal-purple
+                    via-terminal-yellow-dark dark:via-terminal-azure
+                    to-terminal-green-dark dark:to-background-ui-dark
+                    bg-[length:150%_150%] rounded-xl shadow-xl animate-[gradient_3s_linear_infinite]"
+                )
+                .child(
+                    Icon(IconProps::builder().icon(icondata::LuTerminal).build())
+                        .attr("class", "text-foreground dark:text-foreground-dark w-[30vw] h-full -my-3 object-cover"),
+                ),
+            Card(
+                CardProps::builder()
+                    .class(Some("col-span-1 md:col-span-2 w-full"))
+                    .title("Welcome")
+                    .children(Arc::new(|| {
+                        p().class("text-center")
+                            .child("Welcome to my homepage! Feel free to have a look around")
+                            .into_any()
+                    }))
+                    .build(),
+            ),
+            a().class("w-full h-full")
+                .href(based_url("/about"))
+                .child(Card(
+                    CardProps::builder()
+                        .class(None)
+                        .title("About me")
+                        .children(Arc::new(|| {
+                            "Some information about me and this website".into_any()
+                        }))
+                        .build(),
+                )),
+            a().class("w-full h-full")
+                .href(based_url("/projects"))
+                .child(Card(
+                    CardProps::builder()
+                        .class(None)
+                        .title("Projects")
+                        .children(Arc::new(|| {
+                            "Projects I have worked or am still working on".into_any()
+                        }))
+                        .build(),
+                )),
+        ))
+}
