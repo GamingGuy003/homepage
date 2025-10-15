daef2e4575e2b4df49cc766e47bd441eec095dbf
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-15 17:22:42 +0200

    Started implementation on project cards

--- src/components/pages/projects/mod.rs
+++ src/components/pages/projects/mod.rs
@@ -1,8 +1,14 @@
-use leptos::prelude::*;
+use leptos::{html::div, prelude::*};
+
+use crate::components::pages::projects::p1::Project1;
 
 pub mod p1;
+mod project;
+mod project_card;
 
 #[component]
 pub fn Projects() -> impl IntoView {
-    "Projects goes here"
+    div()
+        .class("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3")
+        .child(Project1())
 }
